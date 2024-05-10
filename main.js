eeaacd4d86279eb1bdf5c7338706bbedbc1038f6
Anil Devan <anil.devan@educationadvanced.com>
2024-05-10 17:44:22 -0400

    adding komsos scss

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 import './scss/login.scss';
+import { genCSSVars } from '@eai/kosmos-lib-theme-utils';
+import foundryDarkTheme from './theme/darkTheme.json';
+import foundryLightTheme from './theme/lightTheme.json';
+
+// Generate theme variables
+const darkTheme = `@media (prefers-color-scheme: dark) {${genCSSVars(
+    foundryDarkTheme,
+)}}`;
+const lightTheme = `@media (prefers-color-scheme: light) {${genCSSVars(
+    foundryLightTheme,
+)}}`;
+
+
+function injectThemeVariables(themeCss) {
+    const style = document.createElement("style")
+    style.textContent = themeCss
+    document.head.appendChild(style);
+}
+
+injectThemeVariables(darkTheme)
+injectThemeVariables(lightTheme)
